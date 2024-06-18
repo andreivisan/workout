8df9f3424db694ea598011fc90479a2f2755b5a4
Andrei Visan^ <andrei.m.visan@gmail.com>
2024-06-18 18:03:50 +0200

    Started to code

--- aoc_2023_py/1.py
+++ aoc_2023_py/1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """
+---Part One---
 Something is wrong with global snow production, and you've been selected to take a look. 
 The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
@@ -31,22 +32,55 @@ treb7uchet
 In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
 Consider your entire calibration document. What is the sum of all of the calibration values?
+
+--- Part Two ---
+Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: 
+    one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
 """
+import time
+
+
+str_to_dig = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "for": 4,
+        "five": 5,
+        "sxix"
+        }
 
 # goes through the document line by line and retrieves the first and the last digit
 def parse_input():
+    sum = 0
     with open('input.txt', encoding='utf-8') as f:
         for line in f:
-            print(line)
+            #print(line)
+            #print(get_first_and_last_digit_as_number(line))
+            sum += get_first_and_last_digit_as_number(line)
+    return sum
 
-def get_first_and_last_digit(file_line: str):
-    pass
+def get_first_and_last_digit_as_number(file_line: str):
+    digits = [d for d in file_line if d.isdigit()]
+    if len(digits) == 0: return 0
+    return int(digits[0] + digits[-1])
 
-def sum_numbers():
-    pass
-
+def time_parse_inpu():
+    start_time = time.time()
+    parse_input()
+    end_time = time.time()
+    print(f"time of execution for parse_input is {end_time - start_time} seconds")
 
 if __name__ == "__main__":
-    parse_input()
-    #sum_calibration_values = sum_numbers()
-    #print(f"the sum of all of the calibration numbers is: {sum_calibration_values}")
+    time_parse_inpu()
+    print(f"the sum of all of the calibration numbers is: {parse_input()}")
