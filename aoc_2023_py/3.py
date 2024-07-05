70cfe60845681da7e498f4b3d41e4c0c50749395
andreivisan <andrei.m.visan@gmail.com>
2024-07-05 16:39:16 +0200

    From scratch

--- aoc_2023_py/3.py
+++ aoc_2023_py/3.py
@@ -32,56 +32,21 @@ a symbol and so is a part number; their sum is 4361.
 
 Of course, the actual engine schematic is much larger. What is the sum of all of the part numbers in the engine schematic?
 '''
-import re
 
-input_matrix = []
-number_pattern = re.compile(r'\d+')
 directions = [(-1, -1), (-1, 0), (-1, 1),
               (0, -1),           (0, 1),
               (1, -1),  (1, 0),  (1, 1)]
 
 def part_1():
-    input_matrix = build_matrix()
-    numbers = []
-    for row in range(len(input_matrix)):
-        for col in range(len(input_matrix[0])):
-            if input_matrix[row][col] != 0 and input_matrix[row][col] != 1:
-                if is_adjacent(input_matrix, row, col):
-                    numbers.append(input_matrix[row][col])
-    print(sum(numbers))
-
-def build_matrix(): 
     with open('3.txt', encoding='utf-8') as input_file:
-        for index_ln, line in enumerate(input_file):
-            line_len = len(line)
-            i = 0
-            cols = [] 
-            while i < line_len:    
-                ch = line[i]
-                if ch == '.':
-                    cols.append(0)
-                    i += 1
-                elif not ch.isdigit():
-                    cols.append(1)
-                    i += 1
-                elif ch.isdigit():
-                    match = number_pattern.match(line, i)
-                    if match:
-                        cols.append(int(match.group()))
-                        i += len(match.group())
-            input_matrix.append(cols)
-    return input_matrix
+        lines = [line.strip() for line in input_file]
+        for row_index, line in enumerate(lines):
+            for col_index, ch in enumerate(line):
+                if ch.isdigit():
+                    pass
 
-def is_adjacent(input_matrix, cur_row, cur_col):
-    len_rows = len(input_matrix)
-    len_cols = len(input_matrix[0])
-    for dr, dc in directions:
-        row = cur_row + dr
-        col = cur_col + dc
-        if 0 <= row < len_rows and 0 <= col < len_cols:
-            if input_matrix[dr][dc] == 1:
-                return True
-    return False
+def is_valid(row, col, lines):
+    for dc 
 
 
 if __name__ == "__main__":
