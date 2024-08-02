23982a59c27f2e37e130f10deb4660e75f1b5740
andreivisan <andrei.m.visan@gmail.com>
2024-08-02 13:50:07 +0200

    changed animation

--- algorithms/stack_visualisation.py
+++ algorithms/stack_visualisation.py
@@ -42,14 +42,14 @@ class MemBlock:
         self.text.set_y(self.y_rect + 0.5)  # Adjust text position relative to the rectangle
 
 
-func_param_1_mem_block = MemBlock(None, None, 'Function (Add) Variable 1', 0.2, 9, 0.3, 0.6, 1, 0.5, 9.7)
+func_param_1_mem_block = MemBlock(None, None, 'Function Variable 1 - a' , 0.2, 9, 0.3, 0.6, 1, 0.5, 9.7)
 func_param_1_mem_block.create_rect()
 ani = FuncAnimation(fig, func_param_1_mem_block.update, frames=range(101), interval=20)
 func_param_1_mem_block.animation = ani
 
-func_param_2_mem_block = MemBlock(None, None, 'Function (Add) Variable 2', 0.2, 11, 1.6, 0.6, 1, 0.5, 11.7)
+func_param_2_mem_block = MemBlock(None, None, 'Function Variable 2 - b', 0.2, 11, 1.6, 0.6, 1, 0.5, 11.7)
 func_param_2_mem_block.create_rect()
-ani_2 = FuncAnimation(fig, func_param_2_mem_block.update, frames=range(101), interval=60)
+ani_2 = FuncAnimation(fig, func_param_2_mem_block.update, frames=range(101), interval=30)
 func_param_2_mem_block.animation = ani_2
 
 if __name__ == '__main__':
