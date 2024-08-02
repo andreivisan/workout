8c6ad67b73b4fcb80d3a9007405ff8e96267ff1c
andreivisan <andrei.m.visan@gmail.com>
2024-08-02 14:43:16 +0200

    started valid parentheses

---
+++ algorithms/valid_parentheses.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def is_valid(str):
+    stack = []
+    storage = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+    }
+    for ch in str :
+        if stack and ch in storage.keys():
+            if stack[-1] == storage[ch]:
+                stack.pop()
+            else: 
+                stack.append(storage[ch])
+    return len(stack) == 0
+
+
+if __name__ == "__main__":
+    print(is_valid("([{}])"))
+    print(is_valid("[(])"))
