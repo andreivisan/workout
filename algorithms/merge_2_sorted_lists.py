ad857523ae3e85aa5684d0884654a7324e81b2d6
Andrei Visan^ <andrei.m.visan@gmail.com>
2024-08-05 16:37:38 +0200

    Merge 2 sorted linked lists - Optimisation

--- algorithms/merge_2_sorted_lists.py
+++ algorithms/merge_2_sorted_lists.py
@@ -3,38 +3,21 @@
 from singly_linked_list import Node, SinglyLinkedList
 
 def merge_2_lists(head1, head2):
-    if not head1 and not head2:
-        return None
-    if not head1:
-        return head2
-    if not head2:
-        return head1
-    if head1.data <= head2.data:
-        head_result = head1
-        iter1 = head1.next
-        iter2 = head2 
-    else:
-        head_result = head2
-        iter1 = head1
-        iter2 = head2.next
+    head_result = Node()
     current = head_result
-    while iter1 and iter2:
-        if iter1.data <= iter2.data:
-            current.next = iter1
-            iter1 = iter1.next
+    while head1 and head2:
+        if head1.data <= head2.data:
+            current.next = head1
+            head1 = head1.next
         else:
-            current.next = iter2
-            iter2= iter2.next
+            current.next = head2
+            head2= head2.next
         current = current.next
-    while iter1:
-        current.next = iter1
-        iter1 = iter1.next
-        current = current.next
-    while iter2:
-        current.next = iter2
-        iter2 = iter2.next
-        current = current.next
-    return head_result
+    if head1:
+        current.next = head1
+    else:
+        current.next = head2
+    return head_result.next
 
 if __name__ == "__main__":
     sll1 = SinglyLinkedList()
