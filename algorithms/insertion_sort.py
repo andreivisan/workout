5f9931e263fbb1f291cbdea8c53ce148facbcf8e
Andrei Visan^ <andrei.m.visan@gmail.com>
2024-10-03 20:29:58 +0200

    Updated README - added Insertion Sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -6,18 +6,38 @@ def switch(array: list[int], index: int, sorted_index: int):
     array[sorted_index] = temp
 
 def insertion_sort(array: list[int]) -> list[int]:
-    index = 1
-    while index < len(array):
-        sorted_index =  index - 1
-        sorted_val = array[sorted_index]
-        while sorted_index >= 0 and array[index] < sorted_val:
-            switch(array, index, sorted_index)
-            sorted_index -= 1
-        index += 1
+    n = len(array)
+    for i in range(n-1):
+        prev_index = i
+        key_index = i + 1
+        value_2ct = array[prev_index]
+        key = array[key_index]
+        while key < value_2ct and key_index >= 0 and prev_index >= 0:
+            switch(array, key_index, prev_index)
+            key_index -= 1
+            prev_index -=1
+            value_2ct = array[prev_index]
+            key = array[key_index]
     return array
+
+def insert_sorted(array: list[int], val: int, start: int, end: int) -> list[int]:
+    if start > end:
+        array.insert(start, val)
+        return array
+    
+    mid = start + (end - start) // 2
+    
+    if array[mid] == val:
+        array.insert(mid, val)
+        return array
+    elif val > array[mid]:
+        return insert_sorted(array, val, mid + 1, end)
+    else:
+        return insert_sorted(array, val, start, mid - 1)
 
 if __name__ == "__main__":
     array = [8, 4, 2, 9, 5]
     print(insertion_sort(array))
+    print(insert_sorted(array, 11, 0, len(array) - 1))
 
         
